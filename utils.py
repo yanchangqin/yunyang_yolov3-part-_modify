8f395f0cee66090a920364f2c4f86e387dd7fce1
yanchangqin <55050914+yanchangqin@users.noreply.github.com>
2019-12-26 16:05:41 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,342 @@
+﻿#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : utils.py
+#   Author      : YunYang1994
+#   Created date: 2019-02-28 13:14:19
+#   Description :
+#
+#================================================================
+
+import cv2
+import random
+import colorsys
+import numpy as np
+import tensorflow as tf
+from core.config import cfg
+import os
+import PIL.Image as Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import threading
+#
+def read_class_names(class_file_name):
+    '''loads class name from a file'''
+    names = {}
+    with open(class_file_name, 'r',encoding='UTF-8-sig') as data:
+        for ID, name in enumerate(data):
+            names[ID] = name.strip('\n')
+    return names
+
+
+def get_anchors(anchors_path):
+    '''loads the anchors from a file'''
+    with open(anchors_path) as f:
+        anchors = f.readline()
+    anchors = np.array(anchors.split(','), dtype=np.float32)
+    return anchors.reshape(3, 3, 2)
+
+
+def image_preporcess_train(image, target_size, gt_boxes):
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+
+    ih, iw    = target_size
+    h,  w, _  = image.shape
+
+    scale = min(iw/w, ih/h)
+    nw, nh  = int(scale * w), int(scale * h)
+    image_resized = cv2.resize(image, (nw, nh))
+
+    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
+    dw, dh = (iw - nw) // 2, (ih-nh) // 2
+    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
+    image_paded = image_paded / 255.
+
+    if gt_boxes != []:
+        gt_boxes[:, [0, 2]] = gt_boxes[:, [0, 2]] * scale + dw
+        gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
+        return image_paded, gt_boxes
+
+    else:
+        return image_paded,gt_boxes
+
+def image_preporcess(image, target_size, gt_boxes=None):
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+
+    ih, iw    = target_size
+    h,  w, _  = image.shape
+
+    scale = min(iw/w, ih/h)
+    nw, nh  = int(scale * w), int(scale * h)
+    image_resized = cv2.resize(image, (nw, nh))
+
+    image_paded = np.full(shape=[ih, iw, 3], fill_value=128.0)
+    dw, dh = (iw - nw) // 2, (ih-nh) // 2
+    image_paded[dh:nh+dh, dw:nw+dw, :] = image_resized
+    image_paded = image_paded / 255.
+
+    if gt_boxes is None:
+        return image_paded
+
+    else:
+        gt_boxes[:, [0, 2]] = gt_boxes[:, [0, 2]] * scale + dw
+        gt_boxes[:, [1, 3]] = gt_boxes[:, [1, 3]] * scale + dh
+        return image_paded, gt_boxes
+
+def draw_bbox(image, bboxes, classes=read_class_names(cfg.YOLO.CLASSES), show_label=True):
+    """
+    bboxes: [x_min, y_min, x_max, y_max, probability, cls_id] format coordinates.
+    """
+
+    num_classes = len(classes)
+    image_h, image_w, _ = image.shape
+    hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+
+    random.seed(0)
+    random.shuffle(colors)
+    random.seed(None)
+
+    for i, bbox in enumerate(bboxes):
+        coor = np.array(bbox[:4], dtype=np.int32)
+        fontScale = 0.5
+        score = bbox[4]
+        class_ind = int(bbox[5])
+        bbox_color = colors[class_ind]
+        bbox_thick = int(0.6 * (image_h + image_w) / 600)
+        c1, c2 = (coor[0], coor[1]), (coor[2], coor[3])
+        cv2.rectangle(image, c1, c2, bbox_color, bbox_thick)
+
+        if show_label:
+            bbox_mess = '%s: %.2f' % (classes[class_ind], score)
+            t_size = cv2.getTextSize(bbox_mess, 0, fontScale, thickness=bbox_thick//2)[0]
+            cv2.rectangle(image, c1, (c1[0] + t_size[0], c1[1] - t_size[1] - 3), bbox_color, -1)  # filled
+
+            cv2img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            pilimg = Image.fromarray(cv2img)
+
+            draw.text((c1[0], c1[1] - 15), bbox_mess, (0, 0, 0), font=font)
+            image = cv2.cvtColor(np.array(pilimg), cv2.COLOR_RGB2BGR)
+            cv2.putText(image, bbox_mess, (c1[0], c1[1]-2), cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale, (0, 0, 0), bbox_thick//2, lineType=cv2.LINE_AA)
+
+    return image
+
+
+
+def bboxes_iou(boxes1, boxes2):
+
+    boxes1 = np.array(boxes1)
+    boxes2 = np.array(boxes2)
+
+    boxes1_area = (boxes1[..., 2] - boxes1[..., 0]) * (boxes1[..., 3] - boxes1[..., 1])
+    boxes2_area = (boxes2[..., 2] - boxes2[..., 0]) * (boxes2[..., 3] - boxes2[..., 1])
+
+    left_up       = np.maximum(boxes1[..., :2], boxes2[..., :2])
+    right_down    = np.minimum(boxes1[..., 2:], boxes2[..., 2:])
+
+    inter_section = np.maximum(right_down - left_up, 0.0)
+    inter_area    = inter_section[..., 0] * inter_section[..., 1]
+    union_area    = boxes1_area + boxes2_area - inter_area
+    ious          = np.maximum(1.0 * inter_area / union_area, np.finfo(np.float32).eps)
+
+    return ious
+
+
+
+def read_pb_return_tensors(graph, pb_file, return_elements):
+
+    with tf.gfile.FastGFile(pb_file, 'rb') as f:
+        frozen_graph_def = tf.GraphDef()
+        frozen_graph_def.ParseFromString(f.read())
+
+    with graph.as_default():
+        return_elements = tf.import_graph_def(frozen_graph_def,
+                                              return_elements=return_elements)
+    return return_elements
+
+
+def nms(bboxes, iou_threshold, sigma=0.3, method='nms'):
+    """
+    :param bboxes: (xmin, ymin, xmax, ymax, score, class)
+
+    Note: soft-nms, https://arxiv.org/pdf/1704.04503.pdf
+          https://github.com/bharatsingh430/soft-nms
+    """
+    classes_in_img = list(set(bboxes[:, 5]))
+    best_bboxes = []
+
+    for cls in classes_in_img:
+        cls_mask = (bboxes[:, 5] == cls)
+        cls_bboxes = bboxes[cls_mask]
+
+        while len(cls_bboxes) > 0:
+            max_ind = np.argmax(cls_bboxes[:, 4])
+            best_bbox = cls_bboxes[max_ind]
+            best_bboxes.append(best_bbox)
+            cls_bboxes = np.concatenate([cls_bboxes[: max_ind], cls_bboxes[max_ind + 1:]])
+            iou = bboxes_iou(best_bbox[np.newaxis, :4], cls_bboxes[:, :4])
+            weight = np.ones((len(iou),), dtype=np.float32)
+
+            assert method in ['nms', 'soft-nms']
+
+            if method == 'nms':
+                iou_mask = iou > iou_threshold
+                weight[iou_mask] = 0.0
+
+            if method == 'soft-nms':
+                weight = np.exp(-(1.0 * iou ** 2 / sigma))
+
+            cls_bboxes[:, 4] = cls_bboxes[:, 4] * weight
+            score_mask = cls_bboxes[:, 4] > 0.
+            cls_bboxes = cls_bboxes[score_mask]
+
+    return best_bboxes
+
+
+def postprocess_boxes(pred_bbox, org_img_shape, input_size, score_threshold):
+
+    valid_scale=[0, np.inf]
+    pred_bbox = np.array(pred_bbox)
+
+    pred_xywh = pred_bbox[:, 0:4]
+    pred_conf = pred_bbox[:, 4]
+    pred_prob = pred_bbox[:, 5:]
+
+    # # (1) (x, y, w, h) --> (xmin, ymin, xmax, ymax)
+    pred_coor = np.concatenate([pred_xywh[:, :2] - pred_xywh[:, 2:] * 0.5,
+                                pred_xywh[:, :2] + pred_xywh[:, 2:] * 0.5], axis=-1)
+    # # (2) (xmin, ymin, xmax, ymax) -> (xmin_org, ymin_org, xmax_org, ymax_org)
+    org_h, org_w = org_img_shape
+    resize_ratio = min(input_size / org_w, input_size / org_h)
+
+    dw = (input_size - resize_ratio * org_w) / 2
+    dh = (input_size - resize_ratio * org_h) / 2
+
+    pred_coor[:, 0::2] = 1.0 * (pred_coor[:, 0::2] - dw) / resize_ratio
+    pred_coor[:, 1::2] = 1.0 * (pred_coor[:, 1::2] - dh) / resize_ratio
+
+    # # (3) clip some boxes those are out of range
+    pred_coor = np.concatenate([np.maximum(pred_coor[:, :2], [0, 0]),
+                                np.minimum(pred_coor[:, 2:], [org_w - 1, org_h - 1])], axis=-1)
+    invalid_mask = np.logical_or((pred_coor[:, 0] > pred_coor[:, 2]), (pred_coor[:, 1] > pred_coor[:, 3]))
+    pred_coor[invalid_mask] = 0
+
+    # # (4) discard some invalid boxes
+    bboxes_scale = np.sqrt(np.multiply.reduce(pred_coor[:, 2:4] - pred_coor[:, 0:2], axis=-1))
+    scale_mask = np.logical_and((valid_scale[0] < bboxes_scale), (bboxes_scale < valid_scale[1]))
+
+    # # (5) discard some boxes with low scores
+    classes = np.argmax(pred_prob, axis=-1)
+    scores = pred_conf * pred_prob[np.arange(len(pred_coor)), classes]
+    score_mask = scores > score_threshold
+    mask = np.logical_and(scale_mask, score_mask)
+    coors, scores, classes = pred_coor[mask], scores[mask], classes[mask]
+
+    return np.concatenate([coors, scores[:, np.newaxis], classes[:, np.newaxis]], axis=-1)
+
+
+def evaluate_on_gpu(sess, gpu_nms_op, pred_boxes_flag, pred_scores_flag, y_pred, y_true, num_classes, iou_thresh=0.5,
+                    calc_now=True):
+    '''
+    Given y_pred and y_true of a batch of data, get the recall and precision of the current batch.
+    This function will perform gpu operation on the GPU.
+    '''
+
+    num_images = y_true[0].shape[0]
+    true_labels_dict = {i: 0 for i in range(num_classes)}  # {class: count}
+    pred_labels_dict = {i: 0 for i in range(num_classes)}
+    true_positive_dict = {i: 0 for i in range(num_classes)}
+
+    for i in range(num_images):
+        true_labels_list, true_boxes_list = [], []
+        for j in range(3):  # three feature maps
+            # shape: [13, 13, 3, 80]
+            true_probs_temp = y_true[j][i][..., 5:-1]
+            # shape: [13, 13, 3, 4] (x_center, y_center, w, h)
+            true_boxes_temp = y_true[j][i][..., 0:4]
+
+            # [13, 13, 3]
+            object_mask = true_probs_temp.sum(axis=-1) > 0
+
+            # [V, 80] V: Ground truth number of the current image
+            true_probs_temp = true_probs_temp[object_mask]
+            # [V, 4]
+            true_boxes_temp = true_boxes_temp[object_mask]
+
+            # [V], labels, each from 0 to 79
+            true_labels_list += np.argmax(true_probs_temp, axis=-1).tolist()
+            # [V, 4] (x_center, y_center, w, h)
+            true_boxes_list += true_boxes_temp.tolist()
+
+        if len(true_labels_list) != 0:
+            for cls, count in Counter(true_labels_list).items():
+                true_labels_dict[cls] += count
+
+        # [V, 4] (xmin, ymin, xmax, ymax)
+        true_boxes = np.array(true_boxes_list)
+        box_centers, box_sizes = true_boxes[:, 0:2], true_boxes[:, 2:4]
+        true_boxes[:, 0:2] = box_centers - box_sizes / 2.
+        true_boxes[:, 2:4] = true_boxes[:, 0:2] + box_sizes
+
+        # [1, xxx, 4]
+        pred_boxes = y_pred[0][i:i + 1]
+        pred_confs = y_pred[1][i:i + 1]
+        pred_probs = y_pred[2][i:i + 1]
+
+        # pred_boxes: [N, 4]
+        # pred_confs: [N]
+        # pred_labels: [N]
+        # N: Detected box number of the current image
+        pred_boxes, pred_confs, pred_labels = sess.run(gpu_nms_op,
+                                                       feed_dict={pred_boxes_flag: pred_boxes,
+                                                                  pred_scores_flag: pred_confs * pred_probs})
+        # len: N
+        pred_labels_list = [] if pred_labels is None else pred_labels.tolist()
+        if pred_labels_list == []:
+            continue
+
+        # calc iou
+        # [N, V]
+        iou_matrix = calc_iou(pred_boxes, true_boxes)
+        # [N]
+        max_iou_idx = np.argmax(iou_matrix, axis=-1)
+
+        correct_idx = []
+        correct_conf = []
+        for k in range(max_iou_idx.shape[0]):
+            pred_labels_dict[pred_labels_list[k]] += 1
+            match_idx = max_iou_idx[k]  # V level
+            if iou_matrix[k, match_idx] > iou_thresh and true_labels_list[match_idx] == pred_labels_list[k]:
+                if match_idx not in correct_idx:
+                    correct_idx.append(match_idx)
+                    correct_conf.append(pred_confs[k])
+                else:
+                    same_idx = correct_idx.index(match_idx)
+                    if pred_confs[k] > correct_conf[same_idx]:
+                        correct_idx.pop(same_idx)
+                        correct_conf.pop(same_idx)
+                        correct_idx.append(match_idx)
+                        correct_conf.append(pred_confs[k])
+
+        for t in correct_idx:
+            true_positive_dict[true_labels_list[t]] += 1
+
+    if calc_now:
+        # avoid divided by 0
+        recall = sum(true_positive_dict.values()) / (sum(true_labels_dict.values()) + 1e-6)
+        precision = sum(true_positive_dict.values()) / (sum(pred_labels_dict.values()) + 1e-6)
+
+        return recall, precision
+    else:
+        return true_positive_dict, true_labels_dict, pred_labels_dict
+
+
+
+
